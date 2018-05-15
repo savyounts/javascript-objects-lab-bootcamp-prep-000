94ddcc9e16395cc622271b619949a207242be564
savyounts@gmail.com <savyounts@gmail.com>
2018-05-15 04:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {eggs:3, milk: "1cup", flour: "3cups"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign( object, { [key]: value })
+  Object.assign({}, object, { [key]: value })
   return object
 }
 
